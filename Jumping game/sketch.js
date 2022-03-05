6b08625b7e84dd4bc632015c6a6db163197f5229
sunil-1979 <88713523+sunil-1979@users.noreply.github.com>
2022-03-05 18:32:18 +0530

    Update sketch.js

--- Jumping game/sketch.js
+++ Jumping game/sketch.js
@@ -67,36 +67,43 @@ function draw()
    }
    
    
-    frog.velocityY=1;
-    frog.velocityX=1;
+    frog.velocityY=2;
+    frog.velocityX=2;
  }
-if (frog.y==450)
-{
-  text("Game Over",200,200);
-  ocean.setVelocity(0,0);
-  frog.lifetime=0;
-  gameState="end";
-}
- 
-  if (gameState === "end")
-  {
-    
-    
-    coinGroup.destroyEach();
-    climbersGroup.destroyEach();
-    
-     
-  }  
 
+   
   spawnCoin();
+  
+  
   drawSprites();
   fill("yellow");
   stroke("blue");
   
 
   textSize(20);
-  text("Score: "+score,150,30);
-}
+  text("Score: "+score,230,30);
+
+  if(frog.y>=450)
+  {
+    if(ocean.y>300)
+   {
+    ocean.y=150;
+   }
+    gameState="end";
+  }
+  if (gameState === "end")
+  {
+    coinGroup.destroyEach();
+    climbersGroup.destroyEach();
+    textSize(30);
+    fill("red")
+    text("Game Over",200,200)
+  }
+}  
+
+  
+ 
+
 
 
 // create the coin and climber in the same function
@@ -106,9 +113,9 @@ function spawnCoin()
   if (frameCount % 280 === 0)
    {
     //make the x position of the coin and climber the same
-    climber=createSprite(Math.round(random(10,550)),80,200,50);
+    climber=createSprite(Math.round(random(100,550)),80,200,50);
     climber.addImage("climber",climberImg);
-    climber.setVelocity(0,2);
+    climber.setVelocity(0,3);
     
     climber.scale = 0.5;
     climber.lifetime = 160;
@@ -116,7 +123,7 @@ function spawnCoin()
     
     coin = createSprite(climber.x,20,20,20);
     coin.addImage("coin",coinImg);
-    coin.setVelocity(0,2);
+    coin.setVelocity(0,3);
     
     coin.scale = 0.1;
     
